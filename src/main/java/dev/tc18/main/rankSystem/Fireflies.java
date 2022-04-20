cbc4bde52bc4b99883388d468b92b51378ad5a65
tonicraft18 <cmine726@gmail.com>
2022-04-20 10:52:38 +0200

    adios fireflies

--- src/main/java/dev/tc18/main/rankSystem/Fireflies.java
+++
@@ -1,8 +0,0 @@
-package dev.tc18.main.rankSystem;
-
-public class Fireflies {
-    public static void main(String args[]){
-        String[][] matriz = {{"ElGamesHD","alers16","ant22031","Tonicraft18","TeuTue"},{null,null,null,null,null}};
-        System.out.println(matriz[0].length);
-    }
-}
